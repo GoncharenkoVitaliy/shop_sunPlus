0a1cf7e40ad9ed7cb9fb214b67305d12ec007a86
GoncharenkoVitaliy <Vitalesss.87@gmail.com>
2024-02-21 17:54:24 +0200

    исправление пути к db.json

--- src/components/Main/MainContent/MainContent.tsx
+++ src/components/Main/MainContent/MainContent.tsx
@@ -32,7 +32,7 @@ const MainContent = () => {
       try{
          // const res = await axios.get("http://localhost:3031/product");
 
-         const response = await fetch('/db.json');
+         const response = await fetch('/shop_sunplus/db.json');
          const res = await response.json();
 
          setData(res.products);
