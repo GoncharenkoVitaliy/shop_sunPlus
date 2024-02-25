c393fb5e9d4dcbf8b48cf6880bc91ab0e2f27032
GoncharenkoVitaliy <Vitalesss.87@gmail.com>
2024-02-25 11:54:18 +0200

    Стилизация pages

--- src/Pages/Contacts/Contacts.tsx
+++ src/Pages/Contacts/Contacts.tsx
@@ -4,17 +4,17 @@ const Contacts = () => {
   return (
     <div className={styles.wrapper}>
       <div className="container">
-        <h1>Контакты</h1>
-        <p>Если у вас есть вопросы или нужна помощь, пожалуйста, свяжитесь с нами. Мы готовы помочь вам в любое время.</p>
-        <p>Наш адрес:</p>
-        <p>г. Москва, ул. Ленина, д. 10</p>
-        <p>Телефон:</p>
-        <p>+7 (495) 123-45-67</p>
-        <p>Электронная почта:</p>
-        <p>EMAIL</p>
-        <p>Часы работы:</p>
-        <p>Пн-Пт: 9:00 - 19:00</p>
-        <p>Сб-Вс: выходной</p>
+        <h1 className={styles.title}>Контакты</h1>
+        <p className={styles.text}>Если у вас есть вопросы или нужна помощь, пожалуйста, свяжитесь с нами. Мы готовы помочь вам в любое время.</p>
+        <h4 className={styles.subtitle}>Наш адрес:</h4>
+        <p className={styles.text}>г. Москва, ул. Ленина, д. 10</p>
+        <h4 className={styles.subtitle}>Телефон:</h4>
+        <p className={styles.text}>+7 (495) 123-45-67</p>
+        <h4 className={styles.subtitle}>Электронная почта:</h4>
+        <p className={styles.text}>EMAIL</p>
+        <h4 className={styles.subtitle}>Часы работы:</h4>
+        <p className={styles.text}>Пн-Пт: 9:00 - 19:00</p>
+        <p className={styles.text}>Сб-Вс: выходной</p>
       </div>
     </div>
   )
